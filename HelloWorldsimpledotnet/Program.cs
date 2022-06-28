0622fabc1f59cdb5961fa22664ec3d37837793d5
Dell <Dell@DELLLAPTOP>
2022-06-28 09:47:17 +0530

    Add project files.

---
+++ HelloWorldsimpledotnet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldsimpledotnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !!!!");
+            Console.WriteLine("Enter any keys for EXIT !!!");
+            Console.ReadLine();
+        }
+    }
+}
